d303b6b4a6d86141d1cab39fab81b95a200e4f6d
Jesi0523 <luzpao.gr@gmail.com>
2024-07-12 23:07:34 -0600

    Add more intentions

--- model/actions/actions.py
+++ model/actions/actions.py
@@ -11,19 +11,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-# class ActionHelloWorld(Action):
-
-#     def name(self) -> Text:
-#         return "action_hello_world"
-
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-#         dispatcher.utter_message(text="Hello World!")
-
-#         return []
-
+# Define el primer mensaje que envie el chatbot
 class ActionSessionStart(Action):
 
     def name(self) -> Text:
