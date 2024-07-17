8de8df2bd9948ef7788d3ccd46d6b24b5eb70e72
Jesi0523 <luzpao.gr@gmail.com>
2024-07-17 00:25:23 -0600

    Attempt fallback 2

--- model/actions/actions.py
+++ model/actions/actions.py
@@ -52,13 +52,13 @@ class ActionDetectaPalabrasProhibidas(Action):
         return []
     
 # Se envia un mensaje si el chatbot no puede entender la intencion del usuario
-# class ActionDefaultFallback(Action):
-#     def name(self) -> Text:
-#         return "action_default_fallback"
+class ActionDefaultFallback(Action):
+    def name(self) -> Text:
+        return "action_default_fallback"
 
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
-#         dispatcher.utter_message(text="Lo siento, no entiendo a que te refieres.")
-#         return [] 
+        dispatcher.utter_message(text="Lo siento, no entiendo a que te refieres.")
+        return [] 
